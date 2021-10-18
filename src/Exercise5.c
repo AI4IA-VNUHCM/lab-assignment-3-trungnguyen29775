9e9483aebda6686d9686b744425317f852f992dd
NguyenTranMinhTrungITITIU20332 <91869278+NguyenTranMinhTrungITITIU20332@users.noreply.github.com>
2021-10-18 17:27:11 +0700

    test 5

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -21,20 +21,6 @@ void Ex5(int arr[], int n) {
 				arr[i] = arr[j];
 				arr[j] = t;
 			}
-	int numpos = 0;
-	while (arr[numpos] >= 0)
-	{
-		numpos += 1;
-	}
-	for (int i = 0; i < numpos-1; i++)
-		for (int j = i + 1; j < numpos; j++)
-			if (arr[i] > arr[j])
-			{
-				int t = arr[i];
-				arr[i] = arr[j];
-				arr[j] = t;
-			}
-
 	for (int i = 0; i < n; i++)
 		printf("%d ", arr[i]);
 
